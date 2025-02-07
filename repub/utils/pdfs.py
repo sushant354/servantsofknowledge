674faecec4230ff05afbabf59093961eced8bc2a
Sushant Sinha <sushan354@yahoo.com>
2025-02-07 21:43:48 +0530

    Fixing the page number mangling - replacing pypdf2 with pypdf

--- repub/utils/pdfs.py
+++ repub/utils/pdfs.py
@@ -1,8 +1,9 @@
 import io
 import os
+import logging
 
 import pytesseract
-import PyPDF2
+from pypdf import PdfWriter, PdfReader
 from pdf2image import convert_from_path
 
 def pdf_to_images(inpdf, indir):
@@ -16,13 +17,15 @@ def pdf_to_images(inpdf, indir):
         pagenum += 1
 
 def save_pdf(outfiles, langs, outpdf):
+    logger = logging.getLogger('repub.utils.pdfs')
     outfiles.sort(key = lambda x: x[0])
-    pdf_writer = PyPDF2.PdfWriter()
+    pdf_writer = PdfWriter()
     # export the searchable PDF to searchable.pdf
     for pagenum, outfile in outfiles:
+        logger.info('Adding page %d with file %s to PDF', pagenum, outfile)
         page = pytesseract.image_to_pdf_or_hocr(outfile, extension='pdf', lang =langs)
-        pdf = PyPDF2.PdfReader(io.BytesIO(page))
-        pdf_writer.add_page(pdf.pages[0])
+        reader = PdfReader(io.BytesIO(page))
+        pdf_writer.add_page(reader.get_page(0))
 
     with open(outpdf, "wb") as f:
         pdf_writer.write(f)   
